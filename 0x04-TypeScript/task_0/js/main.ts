d4bdf2d865e3aa679bca4512dc46a43d9da9ea99
yismaw17 <yismawmulaw@yahoo.com>
2023-07-05 13:07:26 +0000

    task script

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,41 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+};
+
+const student1: Student = {
+	firstName: "Catherine",
+	lastName: "Reeves",
+	age: 20,
+	location: Ontario,
+};
+
+const student2: Student = {
+        firstName: "Keanu",
+        lastName: "Hides",
+        age: 25,
+        location: Ottawa,
+};
+
+const studentsList: Array<Student> =[student1, student2];
+
+const tableHtml: HTMLTableElement = document.createElement('table');
+const theadHtml: HTMLTableSectionElement = document.createElement('thead');
+const tbodyHtml: HTMLTableSectionElement = document.createElement('tbody');
+
+const rowHeader: HTMLTableRowElement = theadHtml.insertRow();
+rowHeader.insertCell(0).innerHTML = 'firstName';
+rowHeader.insertCell(1).innerHTML = 'location';
+
+tableHtml.append(theadHtml);
+
+studentsList.forEach(student => {
+  const row: HTMLTableRowElement = tbodyHtml.insertRow();
+  row.insertCell(0).innerHTML = student.firstName;
+  row.insertCell(1).innerHTML = student.location;
+});
+
+tableHtml.append(tbodyHtml);
+document.body.appendChild(tableHtml);
